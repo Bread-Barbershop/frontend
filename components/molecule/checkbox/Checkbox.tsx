37bd80f39ba4171af3926d5e5d01b31b04570f64
Jaden <hyunbin.kim.3376@gmail.com>
2026-01-04 17:58:58 +0900

    [feat] checkbox molecule 컴포넌트 구현 #17

---
+++ components/molecule/checkbox/Checkbox.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { forwardRef, InputHTMLAttributes, ReactNode, useId } from 'react';
+
+import { CheckboxIndicator } from '@/components/atom/checkbox-indicator';
+import { Label } from '@/components/atom/label';
+import { cn } from '@/utils/cn';
+
+import { checkboxVariants } from './Check.style';
+
+interface CheckboxProps extends Omit<
+  InputHTMLAttributes<HTMLInputElement>,
+  'size'
+> {
+  className?: string;
+  children: ReactNode;
+  id?: string;
+  direction?: 'right' | 'left' | 'top' | 'bottom';
+}
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  (
+    {
+      children,
+      id,
+      className,
+      disabled = false,
+      direction = 'right',
+      ...props
+    },
+    ref
+  ) => {
+    const generatedId = useId();
+    const checkboxId = id || generatedId;
+
+    return (
+      <div
+        tabIndex={0}
+        className={cn(checkboxVariants({ direction, disabled }), className)}
+      >
+        <CheckboxIndicator
+          size="sm"
+          ref={ref}
+          id={checkboxId}
+          disabled={disabled}
+          {...props}
+        />
+        <Label
+          htmlFor={checkboxId}
+          className={cn(
+            'select-none',
+            (direction === 'top' || direction === 'bottom') &&
+              'min-h-0 py-0 px-0'
+          )}
+        >
+          {children}
+        </Label>
+      </div>
+    );
+  }
+);
+
+Checkbox.displayName = 'Checkbox';
