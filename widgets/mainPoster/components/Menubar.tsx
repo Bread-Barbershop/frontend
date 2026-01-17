6da22331abd64e1526ba83d3d9f8a5ec46f5f6d2
YooJeong Hwang <ujjh77@gmail.com>
2026-01-18 00:53:40 +0900

    [feat] 리치텍스트 행간, 글자색, 폰트 사이즈, 하이라이팅 기능 추가
    - 하이라이팅 고정값만 되는 문제 해결 필요
    - 하이라이팅시 글자색 벗겨지는 현상 수정 필요

--- widgets/mainPoster/components/Menubar.tsx
+++ widgets/mainPoster/components/Menubar.tsx
@@ -1,17 +1,20 @@
+import { Color } from '@tiptap/extension-text-style';
 import { Editor } from '@tiptap/react';
-import React from 'react';
-import { AiOutlineRedo, AiOutlineUndo } from 'react-icons/ai';
+import React, { useState } from 'react';
+// import { AiOutlineRedo, AiOutlineUndo } from 'react-icons/ai';
 import { BsTypeUnderline } from 'react-icons/bs';
 import {
   RiBold,
   RiItalic,
   RiStrikethrough,
-  RiFontSize,
-  RiMarkPenFill,
+  // RiMarkPenFill,
   RiMenu2Fill,
   RiMenu5Fill,
   RiMenu3Fill,
+  // RiColorFilterAiFill,
 } from 'react-icons/ri';
+
+import { Selector } from '@/components/molecules/selector';
 
 const Button = ({
   onClick,
@@ -34,19 +37,43 @@ const Button = ({
   </button>
 );
 
+type selectorOptions = { label: string; value: string };
+
 function Menubar({ editor }: { editor: Editor | null }) {
+  const [selectedFontSize, setSelectedFontSize] = useState<selectorOptions>();
+  const [selectedLineHeight, setSelectedLineHeight] =
+    useState<selectorOptions>();
+
   if (!editor) {
     return null;
   }
+  // 폰트, 자간, 행간 옵션만드는 유틸로 따로 빼기
+  const fontSize: selectorOptions[] = [];
+  const lineHeight: selectorOptions[] = [];
+  const sizeList = [
+    10, 11, 12, 13, 14, 15, 16, 20, 24, 32, 36, 40, 48, 64, 96, 128,
+  ];
+  const lineHeightList = [
+    1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4,
+    2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.5, 4.0,
+  ];
+
+  sizeList.forEach(size => {
+    const obj = {
+      label: `${size}px`,
+      value: `${size}px`,
+    };
+    fontSize.push(obj);
+  });
+  lineHeightList.forEach(size => {
+    const obj = {
+      label: size.toString(),
+      value: size.toString(),
+    };
+    lineHeight.push(obj);
+  });
 
   const buttons = [
-    {
-      // input으로 입력받기 or select menu로 텍스트 크기 보기 제시
-      icon: <RiFontSize className="size-5" />,
-      onClick: () =>
-        editor.chain().focus().toggleTextStyle().setFontSize('24px').run(),
-      isActive: editor.isActive('fontSize'),
-    },
     {
       icon: <RiMenu2Fill className="size-5" />,
       onClick: () => editor.chain().focus().toggleTextAlign('left').run(),
@@ -61,12 +88,6 @@ function Menubar({ editor }: { editor: Editor | null }) {
       icon: <RiMenu3Fill className="size-5" />,
       onClick: () => editor.chain().focus().toggleTextAlign('right').run(),
       isActive: editor.isActive('textAlign'),
-    },
-    {
-      icon: <RiMarkPenFill className="size-5" />,
-      onClick: () =>
-        editor.chain().focus().toggleHighlight({ color: 'yellow' }).run(),
-      isActive: editor.isActive('highlight'),
     },
     {
       icon: <RiBold className="size-5" />,
@@ -90,18 +111,24 @@ function Menubar({ editor }: { editor: Editor | null }) {
       isActive: editor.isActive('strike'),
       disabled: !editor.can().chain().focus().toggleStrike().run(),
     },
-    {
-      icon: <AiOutlineUndo className="size-5" />,
-      onClick: () => editor.chain().focus().undo().run(),
-      isActive: editor.isActive('undo'),
-      disabled: !editor.can().chain().focus().undo().run(),
-    },
-    {
-      icon: <AiOutlineRedo className="size-5" />,
-      onClick: () => editor.chain().focus().redo().run(),
-      isActive: editor.isActive('redo'),
-      disabled: !editor.can().chain().focus().redo().run(),
-    },
+    // {
+    //   icon: <RiMarkPenFill className="size-5" />,
+    //   onClick: () =>
+    //     editor.chain().focus().toggleHighlight({ color: '#000000' }).run(),
+    //   isActive: editor.isActive('highlight'),
+    // },
+    // {
+    //   icon: <AiOutlineUndo className="size-5" />,
+    //   onClick: () => editor.chain().focus().undo().run(),
+    //   isActive: editor.isActive('undo'),
+    //   disabled: !editor.can().chain().focus().undo().run(),
+    // },
+    // {
+    //   icon: <AiOutlineRedo className="size-5" />,
+    //   onClick: () => editor.chain().focus().redo().run(),
+    //   isActive: editor.isActive('redo'),
+    //   disabled: !editor.can().chain().focus().redo().run(),
+    // },
   ];
 
   return (
@@ -109,6 +136,58 @@ function Menubar({ editor }: { editor: Editor | null }) {
       className="menubarContainer mb-2 flex space-x-2"
       onPointerDown={e => e.preventDefault()}
     >
+      <input
+        type="color"
+        onChange={e => {
+          editor
+            .chain()
+            .focus()
+            .toggleTextStyle(Color)
+            .setColor(e.target.value ?? '#000000')
+            .run();
+        }}
+      />
+      <input
+        type="color"
+        onChange={e => {
+          editor
+            .chain()
+            .focus()
+            .toggleHighlight({ color: e.target.value ?? 'yellow' })
+            .run();
+        }}
+      />
+      {/* 폰트 사이즈 */}
+      <Selector
+        placeholder="14px"
+        options={fontSize}
+        // 폰트 사이즈 여러개 섞였을시 mixed 표시 필요
+        onSelect={option => {
+          editor
+            .chain()
+            .focus()
+            .toggleTextStyle()
+            .setFontSize(option.value ?? '14px')
+            .run();
+          setSelectedFontSize(option);
+        }}
+        selected={selectedFontSize ?? null}
+      />
+      {/* 행간 - 슬라이더로 바꿔야함 */}
+      <Selector
+        placeholder="1.0"
+        options={lineHeight}
+        onSelect={option => {
+          editor
+            .chain()
+            .focus()
+            .toggleTextStyle()
+            .setLineHeight(option.value ?? '1.0')
+            .run();
+          setSelectedLineHeight(option);
+        }}
+        selected={selectedLineHeight ?? null}
+      />
       {buttons.map(({ icon, onClick, isActive, disabled }, index) => (
         <Button
           key={index}
