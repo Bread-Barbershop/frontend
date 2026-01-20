5ed0ef89a7b47c9eaec43df7475a5c4f7f7596a2
Jaden <hyunbin.kim.3376@gmail.com>
2026-01-20 14:13:57 +0900

    [feat] textarea 구현..

--- components/atoms/input/MultiRowInput.style.ts
+++ components/atoms/input/MultiRowInput.style.ts
@@ -1,5 +1,5 @@
 import { cva } from "class-variance-authority";
 
 export const multiRowInputVariants = cva(
-  'max-h-30 h-30 w-full p-3 text-center rounded-lg border border-transparent transition-color duration-100 placeholder:text-text-secondary bg-border-neutral enabled:focus:bg-bg-base enabled:focus:outline-none enabled:focus:border-primary disabled:opacity-50 disabled:cursor-not-allowed resize-none'
+  'flex-shrink-0 min-w-full text-center rounded-lg placeholder:text-text-secondary focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed whitespace-pre-wrap break-all before:text-text-secondary before:content-[attr(data-placeholder)] empty:before:block before:hidden'
 )
