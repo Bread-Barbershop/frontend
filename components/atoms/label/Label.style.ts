d2bacdfbaf1757c8ae4e3225c26cf5dd6b70e62b
Jaden <hyunbin.kim.3376@gmail.com>
2026-01-20 14:15:09 +0900

    [style] Label 컴포넌트 min-w-[57px] 설정

--- components/atoms/label/Label.style.ts
+++ components/atoms/label/Label.style.ts
@@ -1,5 +1,5 @@
 import { cva } from 'class-variance-authority';
 
 export const labelVariants = cva(
-  'min-h-8 py-2 px-1 text-sm leading-4 text-text-primary cursor-inherit'
+  'min-h-8 min-w-[57px] py-2 px-1 text-sm leading-4 text-text-primary cursor-inherit'
 );
