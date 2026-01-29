86aad013d34aa80d2cb025d0677cc285d096ba0d
YooJeong Hwang <ujjh77@gmail.com>
2026-01-29 17:09:58 +0900

    [chore] konva 관련 파일 삭제

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,10 +1,8 @@
-// import MainPoster from '@/widgets/mainPoster';
 import Editor from '@/widgets/mainPoster/components/Editor';
 
 const EditorPage = () => {
   return (
     <main className="w-full h-full flex-center">
-      {/* <MainPoster /> */}
       <Editor />
     </main>
   );
