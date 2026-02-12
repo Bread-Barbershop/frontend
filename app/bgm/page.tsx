0337bc4415170566483d58be4c8b3f579a99af4c
HyoYoung0829 <gydud7300@naver.com>
2026-02-12 22:43:04 +0900

    [feat] 배경음악 컴포넌트 구현 임시저장

---
+++ app/bgm/page.tsx
@@ -0,0 +1,66 @@
+import { Label } from '@/components/atoms/label';
+import { Radio } from '@/components/atoms/radio/Radio';
+import { Checkbox } from '@/components/molecules/checkbox/Checkbox';
+import { NavigationBar } from '@/components/molecules/navigation-bar/NavigationBar';
+
+export default function Page() {
+  return (
+    <main>
+      <div className="flex flex-col gap-1 w-93.75 border rounded-lg px-5">
+        <NavigationBar>배경 음악</NavigationBar>
+
+        {/* 라이오 박스 */}
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center gap-2 text-text-secondary has-[input:checked]:text-black px-1">
+            <div className="p-1.5">
+              <Radio name="bgm" value="preset" />
+            </div>
+            <p>Wedding March N0.12</p>
+            <p>02:58</p>
+          </div>
+          <div className="flex items-center gap-2 text-text-secondary has-[input:checked]:text-black px-1">
+            <div className="p-1.5">
+              <Radio name="bgm" value="preset" />
+            </div>
+            <p>Wedding March N0.12</p>
+            <p>02:58</p>
+          </div>
+          <div className="flex items-center gap-2 text-text-secondary has-[input:checked]:text-black px-1">
+            <div className="p-1.5">
+              <Radio name="bgm" value="preset" />
+            </div>
+            <p>Wedding March N0.12</p>
+            <p>02:58</p>
+          </div>
+          <div className="flex items-center gap-2 text-text-secondary has-[input:checked]:text-black px-1">
+            <div className="p-1.5">
+              <Radio name="bgm" value="preset" />
+            </div>
+            <p>Wedding March N0.12</p>
+            <p>02:58</p>
+          </div>
+          <div className="flex items-center gap-2 text-text-secondary has-[input:checked]:text-black px-1">
+            <div className="p-1.5">
+              <Radio name="bgm" value="preset" />
+            </div>
+            <p>Wedding March N0.12</p>
+            <p>02:58</p>
+          </div>
+        </div>
+
+        {/* 구분선 */}
+        <hr className="border-t border-gray-200" />
+
+        {/* 추가기능 */}
+        <div className="flex gap-2 py-2.5">
+          <Label className="font-bold text-[14px]">추가기능</Label>
+          <Checkbox className="gap-1 pl-1">반복재생</Checkbox>
+          <Checkbox className="gap-1">하이라이트만 재생</Checkbox>
+        </div>
+
+        {/* 컨트롤러 */}
+        <div></div>
+      </div>
+    </main>
+  );
+}
