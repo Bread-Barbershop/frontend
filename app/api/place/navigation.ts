1ab2faa73c17940b63b6f781bf967aabff6f4386
YooJeong Hwang <ujjh77@gmail.com>
2026-02-26 22:07:48 +0900

    [feat] 스토어 연동 로직 주석 삭제

--- app/api/place/navigation.ts
+++ app/api/place/navigation.ts
@@ -8,6 +8,7 @@ function openApp(
     return;
   }
 
+  // eslint-disable-next-line prefer-const
   let timer: ReturnType<typeof setTimeout>;
 
   const handleVisibilityChange = () => {
@@ -55,34 +56,22 @@ export function openNaverMap(lat: number, lng: number, name: string) {
   const url = `nmap://place?lat=${lat}&lng=${lng}&name=${encodeName}&appname=mobile-invitation`;
   const webUrl = `https://map.naver.com/v5/search/${encodeName}`;
   const type = 'naver';
-  // const iosStoreURL = 'itms-apps://itunes.apple.com/app/id311867728?mt=8';
-  // const androidStoreURL =
-  //   'https://play.google.com/store/apps/details?id=com.nhn.android.nmap';
 
   openApp(type, url, webUrl);
-  // openApp(url, webUrl, iosStoreURL, androidStoreURL);
 }
 export function openKakaoMap(lat: number, lng: number, name: string) {
   const encodeName = encodeURIComponent(name);
   const url = `kakaomap://look?p=${lat},${lng}`;
   const webUrl = `https://map.kakao.com/link/to/${encodeName},${lat},${lng}`;
   const type = 'kakao';
-  // const iosStoreURL = 'itms-apps://itunes.apple.com/app/id304608425';
-  // const androidStoreURL =
-  //   'https://play.google.com/store/apps/details?id=com.kakao.map';
 
   openApp(type, url, webUrl);
-  // openApp(url, webUrl, iosStoreURL, androidStoreURL);
 }
 export function openTMap(lat: number, lng: number, name: string) {
   const encodeName = encodeURIComponent(name);
   const url = `tmap://route?rGoName=${encodeName}&rGoX=${lng}&rGoY=${lat}`;
   const webUrl = `https://map.naver.com/v5/search/${encodeName}`;
   const type = 'tmap';
-  // const iosStoreURL = 'itms-apps://itunes.apple.com/app/id431589174';
-  // const androidStoreURL =
-  //   'https://play.google.com/store/apps/details?id=com.skt.tmap';
 
   openApp(type, url, webUrl);
-  // openApp(url, webUrl, iosStoreURL, androidStoreURL);
 }
