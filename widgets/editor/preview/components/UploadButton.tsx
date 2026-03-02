35271532f9b0aa743307d2082d034460f9b48276
YooJeong Hwang <ujjh77@gmail.com>
2026-03-02 21:11:57 +0900

    [fix] 에러 해결

--- widgets/editor/preview/components/UploadButton.tsx
+++ widgets/editor/preview/components/UploadButton.tsx
@@ -4,9 +4,9 @@ import { useShallow } from 'zustand/shallow';
 import { saveInvitationFlow } from '@/app/oauthTest/utils/saveInvitationFlow';
 import { useBgmStore } from '@/components/organisms/bgm/store/useBgmStore';
 import { useEditorStore } from '@/shared/store/editorStore/useEditorStore';
+import { useFabricContext } from '@/widgets/mainPoster/context/FabricContext';
 
 import SaveModal from './SaveModal';
-import { useFabricContext } from '@/widgets/mainPoster/context/FabricContext';
 
 function UploadButton() {
   const [isModalOpen, setIsModalOpen] = useState(false);
