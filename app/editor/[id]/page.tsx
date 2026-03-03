28f8d831bc52a796610afd3fa0fec18879361af3
kimym98 <tampler48@gmail.com>
2026-03-03 16:20:49 +0900

    [chore] lint 에러 수정

--- app/editor/[id]/page.tsx
+++ app/editor/[id]/page.tsx
@@ -1,5 +1,3 @@
-import { cookies } from 'next/headers';
-
 // import {
 //   downloadFiles,
 //   loadInvitations,
@@ -9,7 +7,6 @@ import Preview from '@/widgets/editor/preview/Preview';
 import RightPanel from '@/widgets/editor/rightPanel/RightPanel';
 import { FabricProvider } from '@/widgets/mainPoster/context/FabricContext';
 
-import TestButton from './components/TestButton';
 // export const dynamic = 'force-static';
 // export const revalidate = false;
 
@@ -19,25 +16,9 @@ export default async function EditorPage({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get('access_token')?.value;
-  console.log(accessToken);
 
-  // const response = await fetch(
-  //   `https://www.googleapis.com/drive/v3/files/${id}`,
-  //   {
-  //     headers: {
-  //       Authorization: `Bearer ${accessToken}`,
-  //       Accept: 'application/json',
-  //     },
-  //   }
-  // );
+  console.log(id);
 
-  // if (response.ok) {
-  //   const data = await response.json();
-  //   console.log(data);
-  // } else {
-  //   console.error('에러 발생:', response.status);
   // }
   try {
     // const result = await loadInvitations(id);
@@ -55,7 +36,6 @@ export default async function EditorPage({
   }
   return (
     <FabricProvider>
-      <TestButton id={id} />
       <div className="w-screen h-screen bg-[#E7E9EB] flex flex-col gap-13 justify-center overflow-hidden">
         <div className="flex justify-between items-center">
           <LeftPanel />
