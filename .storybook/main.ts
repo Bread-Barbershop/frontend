653df2a517a1e11f7b94ffe18681cf755b99e264
황유정 <ujjh77@gmail.com>
2026-03-10 17:27:49 +0900

    [fix] 배포 에러 해결

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../components/**/*.stories.@(ts|tsx|js|jsx)'],
@@ -6,6 +7,15 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/nextjs-vite',
     options: {},
+  },
+  viteFinal: async config => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../'),
+      };
+    }
+    return config;
   },
 };
 
