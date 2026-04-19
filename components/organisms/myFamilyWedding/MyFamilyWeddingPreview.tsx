93b7d198880aa32138d55798d67e9aa990d18de0
YooJeong Hwang <ujjh77@gmail.com>
2026-04-19 20:12:07 +0900

    [feat] 신랑 신부 소개 이름과 가족소개의 이름 연동

--- components/organisms/myFamilyWedding/MyFamilyWeddingPreview.tsx
+++ components/organisms/myFamilyWedding/MyFamilyWeddingPreview.tsx
@@ -1,4 +1,5 @@
 import Flower from '@/shared/assets/icons/flower.svg';
+import { useEditorStore } from '@/shared/store/editorStore/useEditorStore';
 import type { EditorBlock } from '@/shared/types/block';
 
 import { MiddlePreviewWrapper } from '../wrapper/MiddlePreviewWrapper';
@@ -16,6 +17,13 @@ export const MyFamilyWeddingPreview = ({
   ...rest
 }: Props) => {
   const { brideFamily, groomFamily } = blockInfo.props;
+  const blocks = useEditorStore(state => state.block);
+
+  const coupleBlock = blocks.find(b => b.component === 'coupleIntroduction') as
+    | EditorBlock<'coupleIntroduction'>
+    | undefined;
+  const groomName = coupleBlock?.props?.groom || '';
+  const brideName = coupleBlock?.props?.bride || '';
 
   return (
     <MiddlePreviewWrapper
@@ -26,25 +34,25 @@ export const MyFamilyWeddingPreview = ({
       childClassName="w-full flex flex-col gap-1"
       {...rest}
     >
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center gap-1">
         {brideFamily?.map((member, index) => (
-          <p key={index} className="flex">
+          <p key={index} className="flex items-center">
             {brideFamily.length > 1 && index !== 0 && '•'}
             {member.flower && <Flower />}
             {member.name}
           </p>
         ))}
-        의 딸
+        <span>의 딸 {brideName}</span>
       </div>
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center gap-1">
         {groomFamily?.map((member, index) => (
-          <p key={index} className="flex">
+          <p key={index} className="flex items-center">
             {groomFamily.length > 1 && index !== 0 && '•'}
             {member.flower && <Flower />}
             {member.name}
           </p>
         ))}
-        의 아들
+        <span>의 아들 {groomName}</span>
       </div>
     </MiddlePreviewWrapper>
   );
