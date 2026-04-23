7cfd762c1bb26b97b5eed1229ee00435a81a7f58
YooJeong Hwang <ujjh77@gmail.com>
2026-04-23 21:12:29 +0900

    [style] 계좌번호 스타일 수정

--- components/organisms/account/preview/AccountsPerGroupPreview.tsx
+++ components/organisms/account/preview/AccountsPerGroupPreview.tsx
@@ -31,30 +31,28 @@ export const AccountsPerGroupPreview = ({
   };
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-3">
       {isOpenAccount &&
         accountList.map((account: Account, j: number) => (
           <div key={j} className="flex items-center gap-2 w-full">
             <Button
               type="button"
               variant="borderless"
-              className="w-8"
+              className="w-8 h-8 flex-center"
               onClick={() => handleCopyAccount(account.bank, account.account)}
             >
               <CopyIcon />
             </Button>
-            <div className="flex flex-col w-full justify-center h-9">
-              <p className="text-sm text-start font-semibold text-border-liner">
+            <div className="flex flex-col w-full justify-center text-start h-[50px]">
+              <p className="text-[13px] text-start font-semibold text-border-liner">
                 {account.name}
               </p>
-              <div className="flex items-center gap-2">
-                <p className="text-sm font-semibold text-border-liner">
-                  {account.bank}
-                </p>
-                <p className="text-sm font-semibold text-border-liner">
-                  {account.account}
-                </p>
-              </div>
+              <p className="text-[13px] font-normal text-border-liner">
+                {account.bank}
+              </p>
+              <p className="text-[13px] font-normal text-border-liner">
+                {account.account}
+              </p>
             </div>
             {account.kakao === true && (
               <Button
