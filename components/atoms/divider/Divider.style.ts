3ee9ea63a3e87ab9a8a067215da04189b08ff849
YooJeong Hwang <ujjh77@gmail.com>
2026-04-26 16:55:43 +0900

    [style] 디바이더 크기 변경

--- components/atoms/divider/Divider.style.ts
+++ components/atoms/divider/Divider.style.ts
@@ -1,6 +1,6 @@
 import { cva, VariantProps } from 'class-variance-authority';
 
-export const dividerVariants = cva('flex flex-col gap-1', {
+export const dividerVariants = cva('flex flex-col gap-1.5', {
   variants: {
     padding: {
       left: 'pl-7',
