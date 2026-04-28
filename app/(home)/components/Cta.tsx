dc3f92e85005c45f7d853ff773578572f38d412a
HyoYoung0829 <gydud7300@naver.com>
2026-04-29 02:50:27 +0900

    [feat] 홈페이지 글래스모피즘 디자인 수정

--- app/(home)/components/Cta.tsx
+++ app/(home)/components/Cta.tsx
@@ -31,7 +31,7 @@ function Cta({ initialIsLoggedIn }: CtaProps) {
       <section className="flex flex-col gap-10 border-none">
         <div
           className="
-          flex w-185 flex-col gap-2 rounded-4xl p-8
+          inline-flex w-fit flex-col gap-2 rounded-4xl p-8
           bg-white/6 backdrop-blur-xs
           border-x border-white/30
           shadow-2xl
@@ -45,12 +45,12 @@ function Cta({ initialIsLoggedIn }: CtaProps) {
             Signature Invitation
           </p>
 
-          <h1 className="text-[64px] font-black leading-tight text-[#121212] select-none">
+          <h1 className="text-[64px] font-black leading-tight text-[#121212] select-none whitespace-nowrap">
             우리의 이야기
             <br />첫 시작은 초대장으로.
           </h1>
 
-          <p className="text-2xl font-medium text-[#121212] select-none">
+          <p className="text-2xl font-medium text-[#121212] select-none whitespace-nowrap">
             폰트·컬러·레이아웃까지, 우리만의 시그니처로 마무리해요.
           </p>
         </div>
@@ -60,7 +60,7 @@ function Cta({ initialIsLoggedIn }: CtaProps) {
           onClick={handleStart}
           disabled={isBusy}
           className="
-          flex h-13.25 w-43.25 items-center justify-center rounded-full
+          ml-8 flex h-13.25 w-43.25 items-center justify-center rounded-full
           bg-[#121212] text-2xl font-medium text-white
           transition-all hover:bg-[#202020] active:scale-95 active:bg-[#0D0D0D] cursor-pointer disabled:opacity-60 disabled:cursor-not-allowed
         "
