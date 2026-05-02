e5b727c536ed71e891b26e7ca0b2a0145231878d
YooJeong Hwang <ujjh77@gmail.com>
2026-05-02 21:27:53 +0900

    [fix] 배포환경에서의 폰트 깨짐 현상 해결
    - 폰트 사전 로딩
    - 초기 에디터 접근시 폰트 등록
    - 커스텀 폰트 정보 분리

---
+++ widgets/mainPoster/constants/fonts.ts
@@ -0,0 +1,26 @@
+export const CUSTOM_FONTS = [
+  {
+    family: 'VT323',
+    url: 'url(https://fonts.gstatic.com/s/vt323/v17/pxiKyp0ihIEF2isfFJXUdVNF.woff2)',
+    weight: 'normal',
+    style: 'normal',
+  },
+  {
+    family: 'Pacifico',
+    url: 'url(https://fonts.gstatic.com/s/pacifico/v22/FwZY7-Qmy14u9lezJ-6H6MmBp0u-.woff2)',
+    weight: 'normal',
+    style: 'normal',
+  },
+  {
+    family: 'Lato',
+    url: 'url(https://fonts.gstatic.com/s/lato/v24/S6u8w4BMUTPHh30AXC-qNiXg7Q.woff2)',
+    weight: '100',
+    style: 'normal',
+  },
+  {
+    family: 'Lato',
+    url: 'url(https://fonts.gstatic.com/s/lato/v24/S6u8w4BMUTPHh30AXC-qNiXg7Q.woff2)',
+    weight: '900',
+    style: 'normal',
+  },
+];
