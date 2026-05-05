67e6b6512b95a9c38bff9d3f8f440f99da0df863
HyoYoung0829 <gydud7300@naver.com>
2026-05-06 00:43:17 +0900

    [feat] 행사 장소 컴포넌트 프리뷰에서 지도 렌더 안되는 문제 해결

--- components/organisms/place/NaverMapScript.tsx
+++ components/organisms/place/NaverMapScript.tsx
@@ -1,17 +1,29 @@
 'use client';
 
 import Script from 'next/script';
+import { useEffect } from 'react';
 
 interface Props {
   onReady?: () => void;
 }
 
+function isNaverMapReady() {
+  return typeof window !== 'undefined' && Boolean(window.naver?.maps);
+}
+
 export function NaverMapScript({ onReady }: Props) {
+  useEffect(() => {
+    if (isNaverMapReady()) {
+      onReady?.();
+    }
+  }, [onReady]);
+
   return (
     <Script
       id="naver-map-sdk"
       strategy="afterInteractive"
       src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_MAP_KEY}&submodules=geocoder`}
+      onLoad={onReady}
       onReady={onReady}
     />
   );
