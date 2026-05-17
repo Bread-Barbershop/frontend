a52b170a93d06a977e761275173c0937ab64a0cb
YooJeong Hwang <ujjh77@gmail.com>
2026-05-17 18:13:30 +0900

    [style] 폰트 크기 변경

--- widgets/mainPoster/components/context-menu/style.ts
+++ widgets/mainPoster/components/context-menu/style.ts
@@ -2,4 +2,4 @@ export const ActiveStyle =
   'hover:bg-status-info-bg active:bg-status-info-bg flex justify-between px-1 rounded transition-colors';
 export const DisabledStyle = 'opacity-50 cursor-not-allowed grayscale';
 export const DisabledShortCutStyle =
-  'opacity-50 cursor-not-allowed grayscale text-[12px]';
+  'opacity-50 cursor-not-allowed grayscale text-[11px]';
