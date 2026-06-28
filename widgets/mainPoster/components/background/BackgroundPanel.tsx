3d9317d78528c5b4ec08dea39c8f4bb2f21a4d90
YooJeong Hwang <ujjh77@gmail.com>
2026-06-28 20:29:08 +0900

    [style] 탭 목록에 컬러 피커 가려지는 현상 수정

--- widgets/mainPoster/components/background/BackgroundPanel.tsx
+++ widgets/mainPoster/components/background/BackgroundPanel.tsx
@@ -4,7 +4,10 @@ import { BackgroundColor } from './BackgroundColor';
 
 export const BackgroundPanel = () => {
   return (
-    <LeftEditorWrapper ariaLabel="배경색 편집" className="pb-0">
+    <LeftEditorWrapper
+      ariaLabel="배경색 편집"
+      className="relative z-20 overflow-visible pb-0"
+    >
       <div className="flex w-full items-center justify-center">
         <BackgroundColor />
       </div>
